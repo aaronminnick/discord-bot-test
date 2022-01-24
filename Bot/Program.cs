2ad0033f75841e0c9c8488cdc91c6bd64ba18295
Aaron Minnick <abminnick@gmail.com>
2022-01-24 09:51:37 -0800

    add some early boilerplate

---
+++ Bot/Program.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+
+namespace Bot
+{
+  public class Program
+  {
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    public async Task MainAsync()
+    {
+      
+    }
+  }
+}
