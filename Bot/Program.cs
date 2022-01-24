9d6d4e3e999dc2eda6c358333d86c6978e14f710
Aaron Minnick <abminnick@gmail.com>
2022-01-24 11:08:30 -0800

    update Program.cs per example

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,35 +1,102 @@
 using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Reflection;
+// using System.Threading;
 using System.Threading.Tasks;
 
 namespace Bot
 {
   public class Program
   {
-    private DiscordSocketClient _client;
+    private readonly DiscordSocketClient _client;
+    private readonly CommandService _commands;
+    private readonly IServiceProvider _services;
 
     public static Task Main(string[] args) => new Program().MainAsync();
 
+    public Program()
+    {
+      _client = new DiscordSocketClient(); //optionally, may configure
+      _commands = new CommandService(); //optionally, may configure
+
+      _client.Log += Log;
+      _commands.Log += Log;
+
+      _services = ConfigureServices();
+    }
+
+    private static IServiceProvider ConfigureServices()
+    {
+      var map = new ServiceCollection();
+        // .AddSingleton(new SomeServiceClass());
+
+      return map.BuildServiceProvider();
+    }
+
+    private static Task Log(LogMessage msg)
+    {
+      Console.WriteLine(msg.ToString());
+      return Task.CompletedTask;
+    }
+
     public async Task MainAsync()
     {
-      _client = new DiscordSocketClient();
-      _client.Log += Log;
+      await InitCommands();
 
-      //get the token from environment variables
-      var token = EnvironmentVariables.Token;
-
-      await _client.LoginAsync(TokenType.Bot, token);
+      await _client.LoginAsync(TokenType.Bot, EnvironmentVariables.Token);
       await _client.StartAsync();
+
+      _client.Ready += () => 
+      {
+        Console.WriteLine("Bot is connected!");
+        return Task.CompletedTask;
+      };
 
       //block task - this keeps bot connected indefinitely until program closes
       await Task.Delay(-1);
     }
 
-    private Task Log(LogMessage msg)
+    private async Task InitCommands()
     {
-      Console.WriteLine(msg.ToString());
-      return Task.CompletedTask;
+      await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+
+      _client.MessageReceived += HandleCommandAsync;
+    }
+
+    private async Task HandleCommandAsync(SocketMessage arg)
+    {
+      // Bail out if it's a System Message.
+        var msg = arg as SocketUserMessage;
+        if (msg == null) return;
+
+        // We don't want the bot to respond to itself or other bots.
+        if (msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot) return;
+        
+        // Create a number to track where the prefix ends and the command begins
+        int pos = 0;
+        // Replace the '!' with whatever character
+        // you want to prefix your commands with.
+        // Uncomment the second half if you also want
+        // commands to be invoked by mentioning the bot instead.
+        if (msg.HasCharPrefix('!', ref pos) /* || msg.HasMentionPrefix(_client.CurrentUser, ref pos) */)
+        {
+            // Create a Command Context.
+            var context = new SocketCommandContext(_client, msg);
+            
+            // Execute the command. (result does not indicate a return value, 
+            // rather an object stating if the command executed successfully).
+            var result = await _commands.ExecuteAsync(context, pos, _services);
+
+            // Uncomment the following lines if you want the bot
+            // to send a message if it failed.
+            // This does not catch errors from commands with 'RunMode.Async',
+            // subscribe a handler for '_commands.CommandExecuted' to see those.
+            //if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+            //    await msg.Channel.SendMessageAsync(result.ErrorReason);
+        }
     }
   }
 }
