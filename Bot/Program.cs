1eed5caba84d8735c331726c0a8a533b62530aa9
Aaron Minnick <abminnick@gmail.com>
2022-01-24 10:11:18 -0800

    bot can go online

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,14 +1,35 @@
+using Discord;
+using Discord.WebSocket;
+using System;
 using System.Threading.Tasks;
 
 namespace Bot
 {
   public class Program
   {
+    private DiscordSocketClient _client;
+
     public static Task Main(string[] args) => new Program().MainAsync();
 
     public async Task MainAsync()
     {
-      
+      _client = new DiscordSocketClient();
+      _client.Log += Log;
+
+      //get the token from environment variables
+      var token = EnvironmentVariables.Token;
+
+      await _client.LoginAsync(TokenType.Bot, token);
+      await _client.StartAsync();
+
+      //block task - this keeps bot connected indefinitely until program closes
+      await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+      Console.WriteLine(msg.ToString());
+      return Task.CompletedTask;
     }
   }
 }
